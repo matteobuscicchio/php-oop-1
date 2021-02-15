024aa6da1da23c0155416c4da0d3eed2972d8363
shini <matteoezio@me.com>
2021-02-15 17:00:44 +0100

    esercizio completo

--- index.php
+++ index.php
@@ -19,6 +19,16 @@ class Developer {
         $this->surname = $surname;
         $this->grade = $grade;
     }
+
+    public function getName(){
+        return $this->name;
+    }
+    public function getsurname(){
+        return $this->surname;
+    }
+    public function getGrade(){
+        return $this->grade;
+    }
 }
 
 $marco = new Developer('Marco', 'Montemagno', 'senior Dev');
@@ -29,3 +39,63 @@ $loredana = new Developer('Loredana', 'Bianchi', 'mid Dev');
 
 $devList = [$marco,$riccardo,$matteo,$adriano,$loredana];
 var_dump($devList);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>php-oop-1</title>
+        <style>
+
+            *{
+                margin: 0 auto;
+                padding: 0;
+                box-sizing: border-box;
+            }
+
+            .container {
+                width: 1070px;
+                height: 100%;
+                margin: 0 auto;
+            }
+
+            .dev_container {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                flex-wrap: wrap;
+                padding: 100px 0;
+            }
+
+            .dev {
+                display: flex;
+                justify-content: flex-start;
+                align-items: center;
+                flex-direction: column;
+                flex-basis: 200px;
+                padding: 20px;
+                margin: 10px 20px;
+                height: 360px;
+                background: rgb(46, 58, 70);
+                text-align: center;
+            }
+
+        </style>
+    </head>
+    <body>
+        
+        <div container="container dev_container">
+            
+            <?php foreach ($devList as $dev) { ?>
+                <div class="dev">
+                    <h3><?php echo $dev->getName(), ' ', $dev->getsurname()?></h3>
+                    <span><?php echo $dev->getGrade()?></span>
+                </div>
+            <?php };?>
+
+        </div>
+    </body>
+</html>
